44cf19a420a07e6b0222680280d5f0351f4f0239
ibmdev <ibmdev102017@gmail.com>
2018-07-29 20:36:04 +0200

    Exemple de test karma pour le module service

---
+++ test/unit/google-analytics-library-angularjs/services/googleAnalyticServiceSpec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('GoogleAnalyticService', function() {
+
+    beforeEach(function() {
+
+    console.log('GoogleAnalyticService Before Each');   
+    });
+      
+    it('Google Analytic Service Test 1 ', function() {
+        console.log('Google Analytic Service Test 1 ');  
+        assert(5<7); 
+    });
+});
