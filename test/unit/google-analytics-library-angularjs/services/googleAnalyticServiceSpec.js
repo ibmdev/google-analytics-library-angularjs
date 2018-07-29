53d3d20276aed6019a5c76e68182120c8ca1509c
ibmdev <ibmdev102017@gmail.com>
2018-07-29 20:44:17 +0200

    Mise à jour test Karma pour le module Service

--- test/unit/google-analytics-library-angularjs/services/googleAnalyticServiceSpec.js
+++ test/unit/google-analytics-library-angularjs/services/googleAnalyticServiceSpec.js
@@ -6,6 +6,10 @@ describe('GoogleAnalyticService', function() {
 
     console.log('GoogleAnalyticService Before Each');   
     });
+
+    afterEach(function() {
+        console.log('GoogleAnalyticService After Each');   
+    });
       
     it('Google Analytic Service Test 1 ', function() {
         console.log('Google Analytic Service Test 1 ');  
